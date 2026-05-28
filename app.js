c71f20689e154f5850cf7d4c4daa0e6629a0c18d
VrindaBhatt2304 <vrindabhatt02@gmail.com>
2026-05-28 11:02:15 +0530

    first commit

--- app.js
+++ app.js
@@ -1,31 +1,46 @@
 const express = require("express");
-const connectDB=require("./config/db.js");
-const authRouter = require("./routes/auth.js");
-const profileRouter = require("./routes/profile.js");
-const requestsRouter = require("./routes/requests.js");  
-const userRouter = require("./routes/user.js");
+
+const http = require("http");
+
+const connectDB=require("./src/config/db.js");
+
+const authRouter = require("./src/routes/auth.js");
+const profileRouter = require("./src/routes/profile.js");
+const requestsRouter = require("./src/routes/requests.js");  
+const userRouter = require("./src/routes/user.js");
+const chatRouter = require("./src/routes/chat.js");
+
 const cors = require("cors");
 const cookieParser = require("cookie-parser");
+const initializeSocket = require("./src/utils/socket.js");
 
 const app = express();
+
 app.use(cors({
     origin: "http://localhost:5173",
     credentials: true,
 }));
+
 app.use(express.json());
 app.use(cookieParser());
+
 
 app.use("/", authRouter);
 app.use("/", profileRouter);
 app.use("/", requestsRouter); 
 app.use("/", userRouter);
+app.use("/", chatRouter);
+
+
+const server = http.createServer(app);
+initializeSocket(server);
+
 
 connectDB().then(()=>{
     console.log("Database connected successfully...");
-    app.listen(3000, ()=>{
+    server.listen(3000, ()=>{
     console.log("Server is successfully listening on port 3000...");
 });
 }).catch((err)=>{
     console.log("Error connecting to database: ", err);
-}
-);
+});
